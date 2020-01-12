45a1e827001913af9c8de0264090dea6962744ba
Ishaan Jain <ishaan160799@gmail.com>
2020-01-12 19:04:50 +0530

    Splash Activity Created!

--- app/src/main/java/com/systemtron/virtualtouristguide/SplashActivity.kt
+++ app/src/main/java/com/systemtron/virtualtouristguide/SplashActivity.kt
@@ -1,12 +1,36 @@
 package com.systemtron.virtualtouristguide
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private var SPLASH_DELAY: Long = 5000
+
+    private val mRunnable: Runnable = Runnable {
+        if (isFinishing) {
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        mDelayHandler = Handler()
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+    }
+
+
+    override fun onDestroy() {
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+        super.onDestroy()
     }
 }
