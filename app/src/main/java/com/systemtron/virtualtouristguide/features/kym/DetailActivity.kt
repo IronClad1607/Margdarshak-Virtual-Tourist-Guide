be159808bd910af3840a7a2458e42ce8342fb0a2
Ishaan Jain <ishaan160799@gmail.com>
2020-01-17 02:00:14 +0530

    Detail Activity Complete

--- app/src/main/java/com/systemtron/virtualtouristguide/features/kym/DetailActivity.kt
+++ app/src/main/java/com/systemtron/virtualtouristguide/features/kym/DetailActivity.kt
@@ -3,6 +3,11 @@ package com.systemtron.virtualtouristguide.features.kym
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmark
 import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmarkDetector
 import com.systemtron.virtualtouristguide.HomeActivity
@@ -18,7 +23,38 @@ class DetailActivity : AppCompatActivity() {
 
         val name = intent.getStringExtra("pass")
 
-        tvDetails.text = name
+        tvMName.text = name
+
+
+        val firebaseDatabse = FirebaseDatabase.getInstance()
+        val myRef = firebaseDatabse.reference
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                showData(p0)
+            }
+
+        })
+    }
+
+    private fun showData(snapshot: DataSnapshot) {
+        for (ds in snapshot.children) {
+            val arch = ds.child(tvMName.text as String).child("arch").value.toString()
+            val history = ds.child(tvMName.text.toString()).child("history").value.toString()
+            val fee = ds.child(tvMName.text.toString()).child("fee").value.toString()
+            val vt = ds.child(tvMName.text.toString()).child("visiting time").value.toString()
+
+            runOnUiThread {
+                tvArch.text = arch
+                tvHistory.text = history
+                tvEntry.text = fee
+                tvVT.text = vt
+            }
+        }
     }
 
 
---
+++ app/src/main/java/com/systemtron/virtualtouristguide/features/kym/model/Data.kt
@@ -0,0 +1,8 @@
+package com.systemtron.virtualtouristguide.features.kym.model
+
+data class Data(
+    val arch: String,
+    val fee: String,
+    val history: String,
+    val vistingTime: String
+)
